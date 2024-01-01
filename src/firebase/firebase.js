289b2d4e47753eb6198b40d354c42ff9f9bd4ba2
Hammad  Haider <105510746+hammad-44@users.noreply.github.com>
2024-01-01 16:20:27 +0500

    Changed A BIt

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-	appId: import.meta.env.VITE_FIREBASE_APP_ID,
-	measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-};
+	apiKey: "AIzaSyCwFjr9i40nJIYLgAqHi45fxPnrkKT5kZc",
+	authDomain: "instaclone-5376c.firebaseapp.com",
+	projectId: "instaclone-5376c",
+	storageBucket: "instaclone-5376c.appspot.com",
+	messagingSenderId: "166461920204",
+	appId: "1:166461920204:web:cea558b09b2ddda6834369",
+	measurementId: "G-1J1YKH5TLN"
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
